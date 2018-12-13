a2e04ec1c9cdb3833df8b4a2bad8b4cd437656dd
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-12-12 18:28:48 -0800

    Code4Life

--- src/extra/Code4Life.java
+++ src/extra/Code4Life.java
@@ -11,19 +11,28 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JApplet;
-
+import javax.swing.JOptionPane;
 public class Code4Life {
+public static void main(String[] args) {
+	
 
 	// 1. Ask the user how many hours they spent coding this week.
-
+String time = JOptionPane.showInputDialog("How many hours did you spend coding this week?");
 	// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
 	// write code instead.
-
+int timeInt = Integer.parseInt(time);
+if(timeInt<=2) {
+	System.out.println("Stop watching YouTube and write code instead.");
+}else if(timeInt>=3 && timeInt<=5) {
+	System.out.println("You are a Code Ninja");
+}else if(timeInt>5) {
+	playBatmanTheme();
+}
 	// 3. If it is greater than or equal to 3 AND less than or equal to 5, tell them
 	// they're a Code Ninja
 
-	// 4. If it is more than 5, call the method below to play the Batman theme song.
-
+	// 4. If it is more than 5, call the method below to play the Boatman theme song.
+}
 	private static void playBatmanTheme() {
 		try {
 			AudioClip sound = JApplet.newAudioClip(Code4Life.class.getResource("batman.wav"));
--- src/nested_loops/CircleRing.java
+++ src/nested_loops/CircleRing.java
@@ -10,16 +10,25 @@ public class CircleRing {
     public static void main(String[] args) throws Exception {
 
         // 1. Make a new Robot
-
+        Robot turtle = new Robot();
 
        //  2. Set your robot’s position to x=150 and y=200
-
+        turtle.moveTo(150, 200);
         // 3. Put the robot's pen down
-
+        turtle.penDown();
         // 4. Set the robot’s speed to 10
-
+        turtle.setSpeed(0);
         // 5. Do everything below here 360 times (use i as the counter)
-
+        for (int i = 0; i < 360; i++) {
+			turtle.move(3);
+			turtle.turn(1);
+			if (i%20==0) {
+				for (int j = 0; j < 360; j++) {
+					turtle.move(1);
+					turtle.turn(1);
+				}
+			}
+		}
 	        // 6. Move the robot 3 pixels
 
        //  7. Turn the robot 1 degree
